b1c69b7112a05ddee9848e31c17f58a7cf6a61be
Khushboo Kachhi <kashishkushwaha03.com>
2026-05-22 01:00:44 +0530

    add user and itinerary models with schema definitions

---
+++ backend/src/models/Itinerary.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const itinerarySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    uploadedFiles: [
+      {
+        type: String,
+      },
+    ],
+
+    extractedData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {},
+    },
+
+    itinerary: {
+      type: mongoose.Schema.Types.Mixed,
+      default: {},
+    },
+
+   shareId: {
+  type: String,
+  default: uuidv4,
+  unique: true,
+}
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Itinerary", itinerarySchema);
